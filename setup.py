064e37246e7d289ca3dd1627ab72b6aec561a670
Skablam <stainlesssteelmatt@gmail.com>
2015-07-05 17:03:11 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='flask-spawn',
-      version='0.1.1',
+      version='0.1.2',
       install_requires=[
             'Click==3.3',
             'Cookiecutter',
