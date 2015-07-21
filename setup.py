385cd806bc66a6d0f58e8bc2d4eb1cf0b6b92d3c
Skablam <stainlesssteelmatt@gmail.com>
2015-07-21 21:54:06 +0100

    fixed tox tests

--- setup.py
+++ setup.py
@@ -2,23 +2,40 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 import sys
 
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+# class Tox(TestCommand):
+#     user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
+#     def initialize_options(self):
+#         TestCommand.initialize_options(self)
+#         self.tox_args = None
+#     def finalize_options(self):
+#         TestCommand.finalize_options(self)
+#         self.test_args = []
+#         self.test_suite = True
+#     def run_tests(self):
+#         import tox
+#         import shlex
+#         args = self.tox_args
+#         if args:
+#             args = shlex.split(self.tox_args)
+#         errno = tox.cmdline(args=args)
+#         sys.exit(errno)
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.tox_args = None
+        self.pytest_args = []
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
         self.test_args = []
         self.test_suite = True
+
     def run_tests(self):
         #import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
+        import pytest
+        errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
 setup(name='flask-spawn',
@@ -38,6 +55,6 @@ setup(name='flask-spawn',
             [console_scripts]
             spawn=flaskspawn.spawn:spawn
       ''',
-      tests_require=['tox'],
-      cmdclass = {'test': Tox},
+      tests_require=['pytest'],
+      cmdclass = {'test': PyTest},
 )
